3de80b6c3a51616b27924dd508d25f63c74286f1
thushiJ <thushijayapathma50@gmail.com>
2024-09-12 02:17:00 +0530

    fix formatting

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,66 +1,63 @@
-import React from 'react'
+import React from "react";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-import { Separator } from '@radix-ui/react-select'
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Separator } from "@radix-ui/react-select";
 import { CiSearch } from "react-icons/ci";
-import { CarMakes, Pricing } from '@/Shared/Data';
+import { CarMakes, Pricing } from "@/Shared/Data";
 
-
- function Search () {
+function Search() {
   return (
-    <div className='p-2 md:p-5 bg-white rounded-md md:rounded-full
+    <div
+      className="p-2 md:p-5 bg-white rounded-md md:rounded-full
     flex-col md:flex md:flex-row gap-10 px-5 items-center
-    w-full md: w-max'> 
-        <Select>
-    <SelectTrigger className="w-[180px] outline-none md:border-none w-full shadow-none text-lg">
-      <SelectValue placeholder="Car Makes" />
-    </SelectTrigger>
-    <SelectContent>
-      {CarMakes.map ((maker, index)=>(
-        <SelectItem value = {maker.name}>{maker.name}</SelectItem>
-      ))}
-    </SelectContent>
-    
-  </Select>
+    w-full md:w-max"
+    >
+      <Select>
+        <SelectTrigger className="w-[180px] outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Car Makes" />
+        </SelectTrigger>
+        <SelectContent>
+          {CarMakes.map((maker, index) => (
+            <SelectItem value={maker.name}>{maker.name}</SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
 
-  <Separator orientation="vertical" className='hidden md:block'/>
-  <Select>
-  <SelectTrigger className= "w-[180px] outline-none md:border-none w-full shadow-none text-lg">
-    <SelectValue placeholder="Pricing" />
-  </SelectTrigger>
-  <SelectContent>
-    {Pricing.map((price, index)=>(
-      <SelectItem value = {price.amount}>{price.amount}</SelectItem>
-    )
-    )}
-  </SelectContent>
-  
-</Select>
+      <Separator orientation="vertical" className="hidden md:block" />
+      <Select>
+        <SelectTrigger className="w-[180px] outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Pricing" />
+        </SelectTrigger>
+        <SelectContent>
+          {Pricing.map((price, index) => (
+            <SelectItem value={price.amount}>{price.amount}</SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
 
-<Separator orientation="vertical" className='hidden md:block'/>
-<Select>
-  <SelectTrigger className= "w-[180px] outline-none md:border-none w-full shadow-none text-lg">
-  <SelectValue placeholder="Cars" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="light">New</SelectItem>
-    <SelectItem value="dark">Old</SelectItem>
-    
-  </SelectContent>
-</Select>
-<div> 
-
-
-<CiSearch className='text-[50px] bg-purple-500
-rounded-full p-3 text-white hover:scale-105 transition-all cursor-pointer'/>
-</div>
-  </div>
-  )
+      <Separator orientation="vertical" className="hidden md:block" />
+      <Select>
+        <SelectTrigger className="w-[180px] outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Cars" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">New</SelectItem>
+          <SelectItem value="dark">Old</SelectItem>
+        </SelectContent>
+      </Select>
+      <div>
+        <CiSearch
+          className="text-[50px] bg-purple-500
+rounded-full p-3 text-white hover:scale-105 transition-all cursor-pointer"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
